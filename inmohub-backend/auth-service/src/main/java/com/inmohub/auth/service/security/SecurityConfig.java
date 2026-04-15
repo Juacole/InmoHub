98c1ea45dd319b47055a029003c2b8b49675227f
Juacole <jvacotunjar@gmail.com>
2026-04-15 14:27:58 +0200

    feat: configuración de seguridad básica y reglas de acceso a endpoints

---
+++ inmohub-backend/auth-service/src/main/java/com/inmohub/auth/service/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.inmohub.auth.service.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * Objeto que define el flujo de cada petición HTTP.
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.
+                csrf(AbstractHttpConfigurer::disable) // Desactivada la proyección contra ataques csrf, el token ya protege de ello
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/users/login", "/api/v1/users/create", "/v3/api-docs/**", "/swagger-ui/**").permitAll() // Rutas públicas
+                        .anyRequest().authenticated() // Demás rutas necesitan un token
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // El servidor no guardara estado de la sesión, cada petición es independiente
+                .build();
+    }
+
+    /**
+     * Objeto encargado de coordinar el proceso de login.
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    /**
+     * Objeto encargado de hashear la contraseña recibida en el login
+     * usando BCrypt.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
