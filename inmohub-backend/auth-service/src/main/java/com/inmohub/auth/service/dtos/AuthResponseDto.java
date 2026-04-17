64ba3074f39a014e64a28eecfcfd4b5f6d105579
Juacole <jvacotunjar@gmail.com>
2026-04-18 01:19:21 +0200

    feat: se agregan validaciones

--- inmohub-backend/auth-service/src/main/java/com/inmohub/auth/service/dtos/AuthResponseDto.java
+++ inmohub-backend/auth-service/src/main/java/com/inmohub/auth/service/dtos/AuthResponseDto.java
@@ -1,6 +1,10 @@
 package com.inmohub.auth.service.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AuthResponseDto(
+        @NotBlank(message = "El token es obligatorio")
         String accessToken,
+        @NotBlank(message = "El refresh token es obligatorio")
         String refreshToken
 ) {}
