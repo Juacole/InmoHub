22aec21bdc44c14fd772618f694138c7ac5caad1
Juacole <jvacotunjar@gmail.com>
2026-05-14 13:51:44 +0200

    feat: agregar configuración para mostrar una pantalla principal en función del rol.

--- inmohub-frontend/composeApp/src/commonMain/kotlin/com/inmohub/frontend/App.kt
+++ inmohub-frontend/composeApp/src/commonMain/kotlin/com/inmohub/frontend/App.kt
@@ -1,16 +1,71 @@
 package com.inmohub.frontend
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.transitions.SlideTransition
+import com.inmohub.frontend.core.network.NetworkClient
+import com.inmohub.frontend.core.themes.inmohubColorScheme
+import com.inmohub.frontend.core.utils.JwtUtils
+import com.inmohub.frontend.features.auth.data.local.SessionManager
+import com.inmohub.frontend.features.auth.data.local.createDataStore
 import com.inmohub.frontend.features.auth.presentation.LoginScreen
+import com.inmohub.frontend.features.lead.presentation.desktop.DashboardScreen
+import com.inmohub.frontend.features.property.presentation.PropertiesListScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        Navigator(LoginScreen())
+    // Singleton
+    // remmber evita nuevas instancias si interfaz se redibuja
+    val dataStore = remember { createDataStore() }
+    val sessionManager = remember { SessionManager(dataStore) }
+
+    // Ejecución de bloque asincrono una sola vez al iniciar aplicación
+    LaunchedEffect(Unit) {
+        NetworkClient.sessionManager = sessionManager
+    }
+
+    val hasSession by sessionManager.isSessionActive.collectAsState(initial = true)
+
+    MaterialTheme(colorScheme = inmohubColorScheme) {
+        if (!hasSession) {
+            Navigator(LoginScreen())
+        } else {
+            var initialScreen by remember { mutableStateOf<Screen?>(null) }
+
+            LaunchedEffect(Unit) {
+                val token = sessionManager.getAccessToken()
+                if (token == null) {
+                    initialScreen = LoginScreen()
+                } else {
+                    val role = JwtUtils.getUserRoleFromToken(token)
+                    initialScreen = when (role) {
+                        "ADMIN", "AGENT" -> DashboardScreen()
+                        "CLIENT", "OWNER" -> PropertiesListScreen()
+                        else -> LoginScreen()
+                    }
+                }
+            }
+
+            if (initialScreen == null) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(color = inmohubColorScheme.secondary)
+                }
+            } else {
+                // let operador seguro de kotlin, ejecuta lo que le pasen
+                initialScreen?.let { Navigator(it) }
+            }
+        }
     }
 }
---
+++ inmohub-frontend/composeApp/src/commonMain/kotlin/com/inmohub/frontend/core/themes/Theme.kt
@@ -0,0 +1,16 @@
+package com.inmohub.frontend.core.themes
+
+import androidx.compose.material3.lightColorScheme
+
+val inmohubColorScheme = lightColorScheme(
+    primary = NavyBluePrimary,
+    secondary = TileOrangeSecondary,
+    background = BackgroundWhite,
+    surface = SurfaceGray,
+    error = ErrorRed,
+    onPrimary = BackgroundWhite,
+    onSecondary = TextDarkGray,
+    onBackground = TextDarkGray,
+    onSurface = TextDarkGray,
+    onError = BackgroundWhite
+)
